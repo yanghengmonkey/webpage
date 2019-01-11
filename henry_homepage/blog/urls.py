75f13708856d74b1337a17ee7f40ca39c2eb6922
yanghengmonkey <yanghengmonkey@icloud.com>
2019-01-11 09:31:22 +0800

    Update local changes

--- henry_homepage/blog/urls.py
+++ henry_homepage/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)$', views.DetailView.as_view(), name='detail'),
 ]
 
--- henry_homepage/blog/views.py
+++ henry_homepage/blog/views.py
@@ -1,6 +1,41 @@
 from django.views import generic
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import  reverse_lazy
 from django.shortcuts import render
+from . models import Post
 
-class IndexView(generic.base.TemplateView):
+class IndexView(ListView):
     template_name = 'blog/index.html'
+    context_object_name = 'all_posts'
 
+    def get_queryset(self):
+        return Post.objects.all()
+
+class DetailView(DetailView, pk):
+    model = Post
+    template_name = 'blog/detail.html'
+
+class PostCreate(CreateView):
+    model = Post
+    fields = [
+         'author'
+        ,'title'
+        ,'text'
+        ,'created_date'
+        ,'published_date'
+    ]
+
+class PostUpdate(UpdateView):
+    model = Post
+    fields = [
+        'author'
+        ,'title'
+        ,'text'
+        ,'created_date'
+        ,'published_date'
+    ]
+
+class PostDelete(DetailView):
+    post = Post
+    sucess_url = reverse_lazy( 'blog:index' )
