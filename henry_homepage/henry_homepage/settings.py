d2067626170a7932b3b7961443fc2a13a43e5c1a
hengyang <hengyang@iluvatar.ai>
2019-01-12 16:45:52 +0800

    Update preface based on bootstrap homepage

--- henry_homepage/henry_homepage/settings.py
+++ henry_homepage/henry_homepage/settings.py
@@ -121,5 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    BASE_DIR,
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL  = '/media/'
